0b258208349e8e643a1d38fdfe8f4330de65e5e7
boss <boss@boss>
2019-11-06 20:38:49 +0530

    my first file to repo

---
+++ file1.c
@@ -0,0 +1,30 @@
+/* WAP for prime number*/
+#include<stdio.h>
+void main()
+{
+int n,i,a=0;
+
+printf("Enter the value \n");
+scanf("%d",&n);
+
+for(i=2;i<n/2;i++)
+{ 
+if((n%i)==0)
+{
+a=1;
+}
+}
+//printf("is not prime no.");
+
+//break;
+ 
+if(a==0) 
+{
+
+printf("is prime no.");
+}
+else 
+printf("is  not prime no.");
+
+}
+
